4302e07eb1a9380f5def8e91569473d9b0150cfb
USS-Shenzhou <969840795@qq.com>
2026-04-18 20:24:55 +0800

    try fix

---
+++ webrtc/write_gn_args.py
@@ -0,0 +1,34 @@
+"""Write GN args file to avoid shell quoting issues across platforms."""
+import os
+import sys
+
+target_os = sys.argv[1]
+target_cpu = sys.argv[2]
+
+args_dir = os.path.join("_build", "src", "out", "Release")
+os.makedirs(args_dir, exist_ok=True)
+
+args = f"""\
+target_os = "{target_os}"
+target_cpu = "{target_cpu}"
+is_debug = false
+is_component_build = false
+rtc_include_tests = false
+rtc_build_examples = false
+rtc_build_tools = false
+rtc_enable_protobuf = false
+rtc_use_x11 = false
+rtc_include_ilbc = false
+rtc_include_opus = false
+rtc_include_internal_audio_device = false
+clang_use_chrome_plugins = false
+symbol_level = 0
+use_thin_lto = true
+"""
+
+args_path = os.path.join(args_dir, "args.gn")
+with open(args_path, "w", encoding="utf-8") as f:
+    f.write(args)
+
+print(f"Wrote {args_path}")
+print(args)
