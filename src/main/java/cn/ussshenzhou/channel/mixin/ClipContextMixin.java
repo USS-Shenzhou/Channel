0a6acc89077e37f7fbfbd33429f7413a59c81cf2
USS-Shenzhou <969840795@qq.com>
2026-07-01 12:53:38 +0800

    try fix

--- src/main/java/cn/ussshenzhou/channel/mixin/ClipContextMixin.java
+++ src/main/java/cn/ussshenzhou/channel/mixin/ClipContextMixin.java
@@ -1,5 +1,6 @@
 package cn.ussshenzhou.channel.mixin;
 
+import cn.ussshenzhou.channel.util.CompatHelper;
 import cn.ussshenzhou.channel.util.ModTags;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.phys.shapes.Shapes;
@@ -9,17 +10,18 @@ import org.spongepowered.asm.mixin.Shadow;
 @Mixin(ClipContext.Block.class)
 enum ClipContextMixin {
     CHANNEL_OUTLINE((state, level, pos, context) -> {
-        if (state.is(ModTags.AUDIO_SOURCE)) {
+        if (state.is(ModTags.AUDIO_SOURCE) || (CompatHelper.MEK_BLOCK_CLASS != null && CompatHelper.MEK_BLOCK_CLASS.isInstance(state.getBlock()))) {
             return Shapes.empty();
         }
         return state.getShape(level, pos, context);
     }),
     CHANNEL_VISUAL((state, level, pos, context) -> {
-        if (state.is(ModTags.AUDIO_SOURCE)) {
+        if (state.is(ModTags.AUDIO_SOURCE) || (CompatHelper.MEK_BLOCK_CLASS != null && CompatHelper.MEK_BLOCK_CLASS.isInstance(state.getBlock()))) {
             return Shapes.empty();
         }
         return state.getVisualShape(level, pos, context);
     });
+
 
     @Shadow
     private ClipContextMixin(ClipContext.ShapeGetter getShape) {
---
+++ src/main/java/cn/ussshenzhou/channel/util/CompatHelper.java
@@ -0,0 +1,16 @@
+package cn.ussshenzhou.channel.util;
+
+public class CompatHelper {
+
+    public static final Class<?> MEK_BLOCK_CLASS;
+
+    static {
+        Class<?> mek;
+        try {
+            mek = Class.forName("mekanism.common.tile.base.TileEntityUpdateable");
+        } catch (ClassNotFoundException e) {
+            mek = null;
+        }
+        MEK_BLOCK_CLASS = mek;
+    }
+}
