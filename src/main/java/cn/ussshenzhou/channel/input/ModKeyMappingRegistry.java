7936dae9f6ad3c986ad18c39e0f3ea8f034af220
USS-Shenzhou <969840795@qq.com>
2026-07-13 23:12:31 +0800

    fix

--- src/main/java/cn/ussshenzhou/channel/input/ModKeyMappingRegistry.java
+++ src/main/java/cn/ussshenzhou/channel/input/ModKeyMappingRegistry.java
@@ -7,14 +7,12 @@ import cn.ussshenzhou.channel.gui.ConfigScreen;
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.components.AbstractTextAreaWidget;
-import net.minecraft.client.gui.components.EditBox;
-import net.minecraft.client.gui.components.MultilineTextField;
 import net.minecraft.resources.Identifier;
 import net.minecraft.util.Util;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.client.event.InputEvent;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 import net.neoforged.neoforge.client.settings.KeyConflictContext;
@@ -52,39 +50,31 @@ public class ModKeyMappingRegistry {
     }
 
     @SubscribeEvent
-    public static void mute(InputEvent.Key event) {
+    public static void switchMute(InputEvent.Key event) {
         var screen = Minecraft.getInstance().screen;
         if (screen != null) {
             return;
         }
-        var cfg = ChannelClientConfig.get();
-        if (event.getKey() == PTT.getKey().getValue() && modifierMatch(event.getModifiers(), PTT.getKeyModifier().codes())) {
-            if (event.getAction() == InputConstants.PRESS || event.getAction() == InputConstants.REPEAT) {
-                switch (cfg.trigger) {
-                    case PUSH_TO_TALK -> ChannelClientConfig.write(c -> c.onAir = true);
-                    case SWITCH -> {
-                        if (Util.getMillis() - lastSwitch >= 100) {
-                            ChannelClientConfig.write(c -> c.onAir = !c.onAir);
-                            lastSwitch = Util.getMillis();
-                        }
-                    }
-                    default -> {
-                    }
-                }
-            } else {
-                if (cfg.trigger == Trigger.PUSH_TO_TALK) {
-                    ChannelClientConfig.write(c -> c.onAir = false);
-                }
+        if (ChannelClientConfig.get().trigger != Trigger.SWITCH) {
+            return;
+        }
+        if (PTT.consumeClick()) {
+            if (Util.getMillis() - lastSwitch >= 100) {
+                ChannelClientConfig.write(c -> c.onAir = !c.onAir);
+                lastSwitch = Util.getMillis();
             }
         }
     }
 
-    public static boolean modifierMatch(int combined, InputConstants.Key[] modifiers) {
-        for (var m : modifiers) {
-            if (combined == m.getValue()) {
-                return true;
-            }
+    @SubscribeEvent
+    public static void pushMute(ClientTickEvent.Pre event) {
+        var cfg = ChannelClientConfig.get();
+        if (cfg.trigger != Trigger.PUSH_TO_TALK) {
+            return;
         }
-        return combined == 0;
+        boolean toWrite = PTT.isDown();
+        if (toWrite != cfg.onAir) {
+            ChannelClientConfig.write(c -> c.onAir = toWrite);
+        }
     }
 }
