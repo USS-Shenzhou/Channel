7e20afc1617fb6e830bfce6abdf5fa3e581e7a0f
USS-Shenzhou <969840795@qq.com>
2026-09-17 14:20:36 -0700

    fix native load

--- src/main/java/cn/ussshenzhou/channel/audio/client/Initializer.java
+++ src/main/java/cn/ussshenzhou/channel/audio/client/Initializer.java
@@ -8,12 +8,23 @@ import cn.ussshenzhou.channel.audio.nativ.NvidiaHelper;
 import cn.ussshenzhou.channel.config.ChannelClientConfig;
 import cn.ussshenzhou.channel.gui.MicConfirmScreen;
 import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.lifecycle.ClientStartedEvent;
 
 /**
  * @author USS_Shenzhou
  */
+@EventBusSubscriber(Dist.CLIENT)
 public class Initializer {
     private static boolean initialized = false;
+
+    @SubscribeEvent
+    public static void initNative(ClientStartedEvent event){
+        WebRTCHelper.init();
+        NvidiaHelper.init();
+    }
 
     public static void init() {
         if (!initialized) {
@@ -29,8 +40,6 @@ public class Initializer {
         if (!initialized) {
             MicManager.init();
             MicReader.init();
-            WebRTCHelper.init();
-            NvidiaHelper.init();
             AudioManager.init();
             initialized = true;
         }
